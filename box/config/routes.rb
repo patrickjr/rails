1a06846c1920e816f8a5d4d6a6475fb8682324cb
patrickjr <patrickjriel@gmail.com>
2016-02-10 09:23:54 -0800

    deleting unused gems and devise

--- box/config/routes.rb
+++ box/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # get 'home/index'
-  devise_for :users
-
   root 'box_users#index'
   resources :box_users, only: [:index, :create, :destroy]
   get 'box_users/manage/:id', :to => 'box_users#manage'
